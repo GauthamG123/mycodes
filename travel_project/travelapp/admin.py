fe05f6c0e4710287a8c927a05181807a30a04a66
Gautham <tve19me060@cet.ac.in>
2024-01-16 12:43:15 +0530

    add

---
+++ travel_project/travelapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Blog_Rent,Team
+
+admin.site.register(Blog_Rent)
+admin.site.register(Team)
---
+++ travel_project/travelapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Blog_Rent,Team
+
+
+# Create your views here.
+def demo(request):
+    obj = Blog_Rent.objects.all()
+    obj1 = Team.objects.all()
+    return render(request,'index.html',{'result':obj,'result1':obj1})
