19633294fd76114b92466e70d7286b26ade53b09
GauthamG123 <152192555+GauthamG123@users.noreply.github.com>
2024-01-16 12:31:46 +0530

    Delete django_projects/travelproject/travel_project directory

--- django_projects/travelproject/travel_project/travelapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Blog_Rent
-
-admin.site.register(Blog_Rent)
--- django_projects/travelproject/travel_project/travelapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from . models import Blog_Rent
-
-
-# Create your views here.
-def demo(request):
-    obj = Blog_Rent.objects.all()
-    return render(request,'index.html',{'result':obj})
